eb5aae0b2420fee2159b81e10860c9e463a41edf
Jemershon <jamesborromeo3@gmail.com>
2025-09-30 21:52:13 +0800

    Fix login issue: Add missing User model import in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ import notificationsRoutes from "./routes/notifications.js";
 import uploadRoutes from "./routes/upload.js";
 import examsRoutes, { setupModels } from "./routes/exams.js";
 import Exam from "./models/Exam.js";
+import User from "./models/User.js";
 
 // Fix __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
